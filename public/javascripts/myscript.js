f38b7cabb734a591e23d6b354751b25b008e6fe5
banahagos <bana.hagos@yahoo.de>
2019-10-02 16:10:43 +0200

    test

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -64,8 +64,8 @@ MyScript.register(editorElement, {
     apiVersion: 'V4',
     server: {
       scheme: 'https',
-      applicationKey: '515131ab-35fa-411c-bb4d-3917e00faf60' , 
-      hmacKey: '54b2ca8a-6752-469d-87dd-553bb450e9ad'
+      applicationKey: applicationKeyMS, 
+      hmacKey: hmacKeyMS
     },
     v4: {
       text: {
--- routes/notes.js
+++ routes/notes.js
@@ -13,7 +13,6 @@ const client = new vision.ImageAnnotatorClient({
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     private_key: `${process.env.GOOGLE_PRIVATE_KEY.split("\\n").join("\n")}`
   }
-  //keyFilename: 'APIKEY.json'
 });
 
 // GET dashboard
