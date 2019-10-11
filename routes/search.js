ff42048147bfaad9757e6f794733d8175e573b9f
banahagos <bana.hagos@yahoo.de>
2019-10-11 12:29:57 +0200

    implements small changes to search such as trimming the query

--- routes/search.js
+++ routes/search.js
@@ -7,30 +7,32 @@ const Tag = require('../models/tag')
 // GET search result
 router.get('/', async (req, res, next) => {
   try {
-    let regex = new RegExp(req.query.tag, 'i');
+    let trimmedQuery = req.query.tag.trim()
+    let regex = new RegExp(trimmedQuery, 'i');
     let tag = await Tag.findOne({ name: regex })
     let tagSearchResult = await Note.find({ tags: tag._id })
       .populate('tags')
       .populate('owner')
       .exec();
-    
-      let isPrivateUser = () => {
-        if(!tagSearchResult[0].owner.public && tagSearchResult && req.query.tag){
-          return true
-        }
-      } 
 
-      let emptySearch = () => {
-        if(!req.query.tag){
-          return true
-        }
+    let isPrivateUser = () => {
+      if (!tagSearchResult[0].owner.public && tagSearchResult && trimmedQuery.length > 0) {
+        return true
       }
+    }
 
-      let isPublicUser = () => {
-        if(tagSearchResult[0].owner.public && req.query.tag){
-          return true
-        }
+    let emptySearch = () => {
+      if (trimmedQuery.length === 0) {
+        return true
       }
+    }
+
+    let isPublicUser = () => {
+      if (tagSearchResult[0].owner.public && trimmedQuery.length > 0) {
+        return true
+      }
+    }
+
     res.render('search/searchResult', { user: req.user, tagSearchResult: tagSearchResult, isPublicUser, isPrivateUser, emptySearch })
   }
   catch (err) {
